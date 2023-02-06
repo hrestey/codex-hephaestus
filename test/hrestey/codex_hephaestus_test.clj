0d7f45bfd9a7e97d6aef2ac02494d310ee43075d
hrestey <hreste@gmail.com>
2023-02-05 23:55:01 -0500

    Initial commit

---
+++ test/hrestey/codex_hephaestus_test.clj
@@ -0,0 +1,7 @@
+(ns hrestey.codex-hephaestus-test
+  (:require [clojure.test :refer :all]
+            [hrestey.codex-hephaestus :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
